60ce3611a7fd6953756a3587616c3265558e1b21
MrCapsLock <mr0capslock@gmail.com>
2022-08-13 19:24:17 +0200

    Feat: Add some new feature

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 from os import environ
 from time import sleep
 
-from src.txt_to_pic import source_pic, text_to_pic, pa
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from txt_to_pic import source_pic, text_to_pic, pa
 from pyrogram import Client, filters
 
 
-app = Client(environ["TG_STORE"],
-    api_id=int(environ["API_ID"]),
-    api_hash=environ["API_HASH"])
+scheduler = AsyncIOScheduler()
+app = Client(
+    environ["TG_STORE"], api_id=int(environ["API_ID"]), api_hash=environ["API_HASH"]
+)
+
 
 @app.on_message(filters.command("pic") & filters.user("me"))
 async def make_pic(client, message):
@@ -25,6 +29,33 @@ async def make_pic(client, message):
     await reply_mes.reply_photo(source_pic / "sticker.png")
     await client.send_document("me", source_pic / "sticker.png")
 
+
+@app.on_message(filters.command("uuid4") & filters.user("me"))
+async def make_uuid(client, message):
+    from uuid import uuid4
+
+    command = message.text.split()[1:]
+    if len(command) > 0 and command[0] == "hex":
+        await message.reply_text(f"`{uuid4().hex}`", quote=True)
+    else:
+        await message.reply_text(f"`{uuid4()}`", quote=True)
+
+
+@app.on_message(filters.command("merge") & filters.user("me"))
+async def merge_messages(client, message):
+    mes_id, deletion_count = message.message_id, 0
+    command = message.text.split()[1:]
+    message_iteration = client.iter_history(
+        message.chat.id, limit=int(command[0]), offset=1
+    )
+    merged_text = list()
+    async for mess in message_iteration:
+        merged_text.append(mess.text)
+        deleted = await mess.delete()
+    await message.delete()
+    await message.reply_text("\n".join(merged_text[::-1]), quote=True)
+
+
 @app.on_message(filters.command("sticker") & filters.user("me"))
 async def make_sticker(client, message):
     reply_mes = message.reply_to_message
@@ -39,6 +70,7 @@ async def make_sticker(client, message):
     text_to_pic(reply_mes.text, name, align=align)
     await reply_mes.reply_sticker(source_pic / "sticker.webp")
 
+
 @app.on_message(filters.command("addsticker") & filters.user("me"))
 async def add_to_sticker(client, message):
     command = message.text.split()[1:]
@@ -50,16 +82,37 @@ async def add_to_sticker(client, message):
     await message.reply_text("Done.")
 
 
+async def send_schedule_message(client, message, text):
+    await message.reply_text(text)
+
+
+@app.on_message(filters.command("schedule") & filters.user("me"))
+async def schedule_message(client, message):
+    command = ' '.join(message.text.split()[1:])
+    if message.reply_to_message is not None:
+        scheduler.add_job(
+            send_schedule_message,
+            CronTrigger.from_crontab(command),
+            args=[client, message, message.reply_to_message.text],
+        )
+        await message.reply_text("Message scheduled.")
+    else:
+        await message.reply_text("You should reply to a message")
+
+
 @app.on_message(filters.command("clear") & filters.user("me"))
 async def delete_messages(client, message):
     mes_id, deletion_count = message.message_id, 0
     command = message.text.split()[1:]
-    message_iteration = client.iter_history(message.chat.id,
-        limit=int(command[0]), offset=1)
+    message_iteration = client.iter_history(
+        message.chat.id, limit=int(command[0]), offset=1
+    )
     async for mess in message_iteration:
         deleted = await mess.delete()
         if deleted:
             deletion_count += 1
     await message.reply_text(f"{deletion_count} messages deleted.")
 
+
+scheduler.start()
 app.run()
--- src/txt_to_pic.py
+++ src/txt_to_pic.py
@@ -8,43 +8,49 @@ base_pic = pa.cwd() / "assets" / "frames" / "semi.png"
 source_pic = pa.cwd() / "assets" / "exports"
 
 
-def text_to_pic(text, writer, font=def_font,
-                font_size=30, pic=base_pic, align="center"):
+def text_to_pic(
+    text, writer, font=def_font, font_size=30, pic=base_pic, align="center"
+):
     text_font = ImageFont.truetype(str(font), size=font_size, encoding="unic")
     writer_font = ImageFont.truetype(str(font), size=15, encoding="unic")
 
     text = "\n".join([textwrap.fill(line, 25) for line in text.split("\n")])
 
-
     with Image.open(base_pic) as picture:
         draw = ImageDraw.Draw(picture)
-        draw.text(xy=gen_xy(text, align)[0],
-                  text=text,
-                  font=text_font,
-                  align=align,
-                  fill="black",
-                  anchor=f"{gen_xy(text, align)[1]}m",
-                  stroke_width=4,
-                  stroke_fill="white",
-                  direction="rtl")
+        draw.text(
+            xy=gen_xy(text, align)[0],
+            text=text,
+            font=text_font,
+            align=align,
+            fill="black",
+            anchor=f"{gen_xy(text, align)[1]}m",
+            stroke_width=4,
+            stroke_fill="white",
+            direction="rtl",
+        )
 
-        draw.text(xy=(512 / 2, 512 - 70),
-                  text=writer,
-                  font=writer_font,
-                  anchor=f"mm",
-                  fill="black",
-                  stroke_width=2,
-                  stroke_fill="white",
-                  direction="rtl")
-        draw.text(xy=(512 - 80, 60),
-                  text="سخن بزرگان:",
-                  font=writer_font,
-                  align="right",
-                  fill="brown",
-                  anchor="rm",
-                  stroke_width=2,
-                  stroke_fill="white",
-                  direction="rtl")
+        draw.text(
+            xy=(512 / 2, 512 - 70),
+            text=writer,
+            font=writer_font,
+            anchor=f"mm",
+            fill="black",
+            stroke_width=2,
+            stroke_fill="white",
+            direction="rtl",
+        )
+        draw.text(
+            xy=(512 - 80, 60),
+            text="سخن بزرگان:",
+            font=writer_font,
+            align="right",
+            fill="brown",
+            anchor="rm",
+            stroke_width=2,
+            stroke_fill="white",
+            direction="rtl",
+        )
     picture.save(source_pic / "sticker.png")
     picture.save(source_pic / "sticker.webp", "webp")
 
@@ -63,5 +69,4 @@ def gen_xy(text, mode="center"):
     elif mode == "left":
         return ((X_MAR, Y_MAR), "l")
     elif mode == "right":
-        return ((DIMEN-X_MAR, Y_MAR), "r")
-
+        return ((DIMEN - X_MAR, Y_MAR), "r")
